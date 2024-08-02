4b143212554faad3ef8d9ef7a60a5d99d91594b7
yalayo <60822416+yalayo@users.noreply.github.com>
2024-08-02 19:22:38 +0000

    Added example code to render html on the server side.

--- development/src/api.clj
+++ development/src/api.clj
@@ -1,4 +1,5 @@
 (ns api (:require [io.pedestal.http :as http]
+									[hiccup2.core :as h]
 									[environ.core :refer [env]]
 									[logic :refer [vacation-plans holidays]])
 	(:gen-class))
@@ -10,10 +11,23 @@
 
 (defn get-holidays [_] {:status 200 :body holidays})
 
+(defn ok [body]
+	{:status 200
+	 :headers {"Content-Type" "text/html"}
+	 :body body})
+
+(defn template [html-body]
+	(str (h/html [:html [:body (h/raw html-body)]])))
+
+(defn respond-hello [request]
+	(let [name (get-in request [:query-params :name] "World")]
+		(ok (template (str (h/html [:h1 {} "Hello, " name "!"]) )))))
+
 ;; Routes
 (def routes #{["/" :get hello-world :route-name :hello-world]
 	["/vacations" :get get-vacations :route-name :get-vacations]
-	["/holidays" :get get-holidays :route-name :get-holidays]})
+	["/holidays" :get get-holidays :route-name :get-holidays]
+	["/greet" :get respond-hello :route-name :greet]})
 
 
 (def service-map {::http/routes routes
