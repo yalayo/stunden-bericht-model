87e9feba37158dd3514600044d125af1b336385a
Yunior Alayo <yunior.alayo@gmail.com>
2024-08-06 14:01:04 +0200

    Chages to use htmx. Added some example code.

--- development/src/api.clj
+++ development/src/api.clj
@@ -15,7 +15,7 @@
 
 (defn ok [body]
 	{:status 200
-	 :headers {"Content-Type" "text/html"}
+	 :headers {"Content-Type" "text/html" "Content-Security-Policy" "img-src 'self'"}
 	 :body (-> body
             (h/html)
             (str))})
@@ -24,11 +24,28 @@
 	[:html
  	 [:head
       [:title "Title"]
-      (hp/include-js "https://cdn.tailwindcss.com" "https://unpkg.com/htmx.org@1.9.4?plugins=forms")]
+ 	  [:link {:href "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" :rel "stylesheet"}]
+ 	  [:script {:src "htmx.min.js"}]]
      [:body (h/raw html-body)]])
 
+(defn item-component [{:keys [id title description author]}]
+  [:article.p-6.even:bg-white.odd:bg-slate-100.sm:p-8
+   [:h2.break-all.text-lg.font-medium.sm:text-xl
+    [:a.hover:underline
+     {:href (str "htmx/infinite-scroll/item/" id)}
+     title]]
+   [:p.mt-1.break-all.text-sm.text-gray-700
+    description]
+   [:div.mt-4.text-xs.font-medium.text-gray-500
+    [:div.flex.items-center.gap-2
+     [:span.relative.flex.h-10.w-10.shrink-0.overflow-hidden.rounded-full
+      [:img.aspect-square.h-full.w-full
+       {:src (:picture author "https://avataaars.io/?hairColor=BrownDark")}]]
+     [:span (:name author)]]]])
+
 (defn htmx-test []
-  [:div {:class "container mx-auto mt-10"} "Prueba"])
+  [:div {:class "container mx-auto mt-10"}
+   (item-component {:id "id" :title "Title" :description "Desc" :author {:name "Author"}})])
 
 (defn respond-hello [request]
 	(ok (template (str (h/html (htmx-test))))))
@@ -44,6 +61,7 @@
 						 ::http/type   :immutant
 						 ::http/host   "0.0.0.0"
 						 ::http/join?  false
+             ::http/resource-path  "public"
 						 ::http/port   (Integer. (or (env :port) 5000))}) ; Service map
 
 (defn -main [& args] (-> service-map http/create-server http/start)) ; Server Instance
